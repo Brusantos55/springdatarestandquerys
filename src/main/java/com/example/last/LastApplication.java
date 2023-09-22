4f98e2c5c3d8c9f522f2075ae4173465476802da
Bruno Santos <bsantos@soincon.es>
2023-09-22 14:05:26 +0200

    fix: set UTC time zone
    -fix diference between date send and saved when creating

--- src/main/java/com/example/last/LastApplication.java
+++ src/main/java/com/example/last/LastApplication.java
@@ -1,10 +1,20 @@
 package com.example.last;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LastApplication {
+
+	// configura todas las operaciones con fechas del back a UTC
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LastApplication.class, args);
